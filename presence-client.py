63f005386946de2e50d9f924826a6070b32a7af7
elsorino <elso@keemail.me>
2022-06-19 20:21:34 -0400

    Moved fallback URL to config file

--- presence-client.py
+++ presence-client.py
@@ -1,25 +1,20 @@
-import sys
-import json
-import argparse
 import socket
 import struct
 import time
 import re
-from unicodedata import name
 import requests
 import configparser
+from unicodedata import name
 from pypresence import Presence
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
-ip = config['main']['ip']
+switch_ip = config['main']['ip']
 clientid = config['main']['clientid']
+fallbackurl = config['main']['fallback']
 
 TCP_PORT = 0xCAFE
 PACKETMAGIC = 0xFFAADD23
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--ignore-home-screen', dest='ignore_home_screen', action='store_true', help='Don\'t display the home screen. Defaults to false if missing this flag.')
 
 #Defines a title packet
 class Title:
@@ -29,20 +24,16 @@ class Title:
         enc_data = unpacker.unpack(raw_data)
         self.magic = int(enc_data[0])
         #print(enc_data)
+        self.pid = int(enc_data[1])
+        self.name = enc_data[4].decode('utf-8', 'ignore').split('\x00')[0]
+        self.url = "https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/v1/ncom/en_US/games/switch/%s/%s-switch/hero" %(self.name[0], format(self.name).replace(' ', '-'))
+        test_url = requests.get(self.url)
+        if test_url.status_code == 404:
+            self.url = str(fallbackurl)
         if int(enc_data[3]) == 0:
-            self.pid = int(enc_data[1])
             self.name = 'Home Menu'
-            self.url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Nintendo_Switch_Logo_%28without_text%29.svg/480px-Nintendo_Switch_Logo_%28without_text%29.svg.png'
-        else:
-            self.pid = int(enc_data[1])
-            self.name = enc_data[4].decode('utf-8', 'ignore').split('\x00')[0]
-            self.url = "https://assets.nintendo.com/image/upload/ar_1.0,c_scale,g_north_west,w_350/v1/ncom/en_US/games/switch/%s/%s-switch/hero" %(self.name[0], format(self.name).replace(' ', '-'))
-
 
 def main():
-    consoleargs = parser.parse_args()
-    switch_ip = ip
-
     if not checkIP(switch_ip):
         print('Invalid IP')
         exit()
@@ -72,7 +63,7 @@ def main():
         try:
             data = sock.recv(628)
         except:
-            print('Could not connect to Server! Retrying...')
+            print('Could not connect to Switch! Retrying...')
             startTimer = 0
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
@@ -88,22 +79,17 @@ def main():
             if lastProgramName != title.name:
                 startTimer = int(time.time())
                 print("Now playing", title.name)
-            if consoleargs.ignore_home_screen and title.name == 'Home Menu':
-                rpc.clear()
-            else:
-                smallimagetext = ''
-                largeimagekey = ''
-                details = ''
-                largeimagetext = title.name
-                if int(title.pid) != PACKETMAGIC:
-                    smallimagetext = 'SwitchPresence-Rewritten'
-                    largeimagekey = iconFromPid(title.pid)
-                    details = 'Playing ' + str(title.name)
-                if not title.name:
-                    title.name = ''
-                lastProgramName = title.name
-                rpc.update(details=details, start=startTimer, large_image=title.url,
-                        large_text=largeimagetext, small_text=smallimagetext)
+            smallimagetext = ''
+            details = ''
+            largeimagetext = title.name
+            if int(title.pid) != PACKETMAGIC:
+                smallimagetext = 'SwitchPresence-Rewritten'
+                details = 'Playing ' + str(title.name)
+            if not title.name:
+                title.name = ''
+            lastProgramName = title.name
+            rpc.update(details=details, start=startTimer, large_image=title.url,
+                    large_text=largeimagetext, small_text=smallimagetext)
             time.sleep(1)
         else:
             print("Closing...")
